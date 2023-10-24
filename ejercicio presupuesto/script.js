d73d82c234d671951547746e4a69e1814ed2d49a
Brayan Duran <brayan.duranx7@gmail.com>
2023-10-24 02:01:16 -0500

    tng sueño

--- ejercicio presupuesto/script.js
+++ ejercicio presupuesto/script.js
@@ -1,5 +1,33 @@
 
 function agregar(){
-  let nombre = document.getElementById("nombre").ariaValueMax;
+  let cre = document.createDocumentFragment();
+  let div = document.createElement("div");
+  div.setAttribute("class","liss")
+  let div1 = document.createElement("div");
+  let div2 = document.createElement("div");
   
+
+  div1.textContent="ropa"
+  div2.textContent="123"
+  let buttom = document.createElement("button");
+  buttom.setAttribute("class","bu")
+  buttom.textContent="Borrar"
+
+  div.appendChild(div1);
+  div.appendChild(div2);
+  div.appendChild(buttom);
+  cre.appendChild(div);
+  document.getElementById("list").appendChild(cre);
 }
+
+function agpr(){
+  let pre = document.getElementById("pre").value;
+  let pre2 = pre;
+
+
+  document.getElementById("rpre1").textContent=`Presupuesto:$ ${pre2}`;
+  document.getElementById("rpre2").textContent="Restante:$"+pre2;
+  document.getElementById("alert").style.zIndex=-1;
+}
+
+
